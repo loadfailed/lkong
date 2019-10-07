1ec8f8aff286d5d70a9548e8c7472890372158f5
marcos <190766630@qq.com>
2019-10-07 18:44:59 +0800

    commit

--- main.js
+++ main.js
@@ -3,24 +3,24 @@ import App from './App'
 
 Vue.config.productionTip = false
 
-Vue.prototype.checkLogin()=function(backpage,backtype){
-	let SUID = uni.getStorageSync('SUID')
-	let SRAND = uni.getStorageSync('SRAND')
-	let SNAME = uni.getStorageSync('SNAME')
-	let SFACE = uni.getStorageSync('SFACE')
-	
-	if(SUID ==='' || SRAND ===' ' || SFACE===''){
-		uni.redirectTo({
-			  url:"../login/login?backpage="+backpage+"&backtype="+backtype
-		})
-		return false
-	}
-	return [SUID,SRAND,SNAME,SFACE]
+Vue.prototype.checkLogin() = function (backpage, backtype) {
+  let SUID = uni.getStorageSync('SUID')
+  let SRAND = uni.getStorageSync('SRAND')
+  let SNAME = uni.getStorageSync('SNAME')
+  let SFACE = uni.getStorageSync('SFACE')
+
+  if (SUID === '' || SRAND === ' ' || SFACE === '') {
+    uni.redirectTo({
+      url: "../login/login?backpage=" + backpage + "&backtype=" + backtype
+    })
+    return false
+  }
+  return [SUID, SRAND, SNAME, SFACE]
 }
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
